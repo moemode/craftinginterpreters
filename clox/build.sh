038da556a73dc8220952531c4ba0c66da85ecb57
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2022-12-28 21:30:27 +0100

    Work through 14.5 'Constants'

--- clox/build.sh
+++ clox/build.sh
@@ -3,4 +3,5 @@ gcc -c -o main.o main.c
 gcc -c -o memory.o memory.c
 gcc -c -o debug.o debug.c
 gcc -c -o chunk.o chunk.c
-gcc -o clox main.o memory.o chunk.o debug.o -L.
+gcc -c -o value.o value.c
+gcc -o clox main.o memory.o chunk.o debug.o value.o -L.
--- clox/main.c
+++ clox/main.c
@@ -4,9 +4,11 @@
 #include "stdio.h"
 
 int main(int argc, const char *argv[]) {
-    printf("hi");
     Chunk chunk;
     initChunk(&chunk);
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT);
+    writeChunk(&chunk, constant);
     writeChunk(&chunk, OP_RETURN);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
