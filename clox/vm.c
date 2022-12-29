637694a465b39eaf59ca5db038381c3501c051b0
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2022-12-29 16:17:11 +0100

    Add run function

--- clox/vm.c
+++ clox/vm.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "common.h"
 #include "vm.h"
 
@@ -7,4 +8,32 @@ void initVM() {
 }
 
 void freeVM() {
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for(;;) {
+        uint8_t instruction;
+        switch(instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+        }
+    }    
+#undef READ_CONSTANT
+#undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
 }
