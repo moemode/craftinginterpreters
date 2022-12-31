776b2f32138e2ec8d056c5dbfaddcba2acbee9d7
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2022-12-31 17:28:27 +0100

    Add binary arithmetic operators. Finish chapter 15.

--- clox/debug.c
+++ clox/debug.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
 #include "debug.h"
 #include "value.h"
+#include <stdio.h>
 
-static int simpleInstruction(const char* name, int offset) {
+static int simpleInstruction(const char *name, int offset) {
     printf("%s\n", name);
     return offset + 1;
 }
 
-static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
     uint8_t constant = chunk->code[offset + 1];
     printf("%-16s %4d '", name, constant);
     printValue(chunk->constants.values[constant]);
     printf("'\n");
-    return offset + 2;   
+    return offset + 2;
 }
 
 void disassembleChunk(Chunk *chunk, const char *name) {
@@ -33,6 +33,14 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     switch (instruction) {
     case OP_CONSTANT:
         return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_ADD:
+        return simpleInstruction("OP_ADD", offset);
+    case OP_SUBTRACT:
+        return simpleInstruction("OP_SUBTRACT", offset);
+    case OP_MULTIPLY:
+        return simpleInstruction("OP_MULTIPLY", offset);
+    case OP_DIVIDE:
+        return simpleInstruction("OP_DIVIDE", offset);
     case OP_NEGATE:
         return simpleInstruction("OP_NEGATE", offset);
     case OP_RETURN:
@@ -42,4 +50,3 @@ int disassembleInstruction(Chunk *chunk, int offset) {
         return offset + 1;
     }
 }
-
--- clox/main.c
+++ clox/main.c
@@ -8,12 +8,27 @@ int main(int argc, const char *argv[]) {
     initVM();
     Chunk chunk;
     initChunk(&chunk);
+    int constant1 = addConstant(&chunk, 1.2);
+    int constant2 = addConstant(&chunk, 3.4);
+    int constant3 = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant1, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant2, 1);
+    writeChunk(&chunk, OP_ADD, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant3, 1);
+    writeChunk(&chunk, OP_DIVIDE, 1);
+    writeChunk(&chunk, OP_NEGATE, 1);
+    writeChunk(&chunk, OP_RETURN, 1);
+    /*
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
+    */
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
