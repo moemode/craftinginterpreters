bccda6cb1d65d853b18041098117a1e8c2453964
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2022-12-31 18:24:11 +0100

    Adds scanner, compiler skeleton. Finish 16.1

--- clox/build.sh
+++ clox/build.sh
@@ -5,4 +5,6 @@ gcc -c -o debug.o debug.c
 gcc -c -o chunk.o chunk.c
 gcc -c -o value.o value.c
 gcc -c -o vm.o vm.c
-gcc -o clox main.o memory.o chunk.o debug.o value.o vm.o -L.
+gcc -c -o scanner.o scanner.c
+gcc -c -o compiler.o compiler.c
+gcc -o clox main.o memory.o chunk.o debug.o value.o vm.o scanner.o compiler.o -L.
