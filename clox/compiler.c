e7b2b76a6897a215d1c38e826ee069d0aad6d53c
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2023-01-01 18:09:08 +0100

    Add byte emission. Finish 17.3

--- clox/compiler.c
+++ clox/compiler.c
@@ -12,6 +12,12 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
 
 static void errorAt(Token *token, const char *message) {
     if(parser.panicMode) {
@@ -55,16 +61,35 @@ static void consume(TokenType type, const char* message) {
     errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);   
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
 static void expression() {
     return;
 }
 
 bool compile(const char *source, Chunk *chunk) {
     initScanner(source);
+    compilingChunk = chunk;
     parser.hadError = false;
     parser.panicMode = false;
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
 }
