0dd5c2cd4004502d5d5e47be92e237b9c18ab584
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2023-01-01 13:12:15 +0100

    Adds scanner frontend. Finishes 17.2

--- clox/vm.c
+++ clox/vm.c
@@ -1,7 +1,7 @@
 #include "vm.h"
 #include "common.h"
-#include "debug.h"
 #include "compiler.h"
+#include "debug.h"
 #include <stdio.h>
 
 VM vm;
@@ -78,7 +78,20 @@ static InterpretResult run() {
 #undef READ_BYTE
 }
 
-InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+InterpretResult interpret(const char *source) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
